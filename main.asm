cfd374ceddee3cda1b555dafc3d13e8ad1c0c91a
DinoNuggetz1 <krishm2210@gmail.com>
2026-04-12 11:21:06 -0400

    Assembly header for grub multiboot 1

--- main.asm
+++ main.asm
@@ -1,32 +1,15 @@
-[BITS 16]
-[org 7c00]
+[bits 32]
+section .text
+        align 4
+        dd 0x1BADB002              ; Multiboot magic
+        dd 0x00                    ; Flags
+        dd -(0x1BADB002 + 0x00)    ; Checksum
 
-KERNEL_OFFSET equ 0x1000
+global _start
+extern kernel_main
 
-mov [BOOT_DRIVE], dl
-
-mov bp 0x9000
-mov sp, bo
-
-call lod_kernl
-
-jmp $
-
-[BITS 16]
-lod_kernl:
-mov bx, KERNEL_OFFSET
-mov dh, 2
-mov dl, [BOOT_DRIVE]
-call dsk_lod
-ret
-[BITS 32]
-BEGIN32BIT:
-call KERNEL_OFFSET
-jmp $
-
-BOOT_DRIVE db 0
-
-times 510 - ($-$$) db 0
-
-; magic number
-dw 0xaa55
+_start:
+        call kernel_main           ; Jump to your or my C code
+.hang:
+    hlt          
+    jmp .hang 
